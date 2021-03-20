d70b134a6494aa366eec1c14c7f85392f593a5fe
Reid <rlj0713@gmail.com>
2021-03-20 14:31:11 -0400

    commit tree fb8937095a974b3cc6e4cd521c3fbdb7469764f8

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    belongs_to :network
+
+    def actors_list
+        array = []
+        self.characters.each do |c|
+            array << "#{c.actor.first_name} #{c.actor.last_name}"
+        end
+        array
+    end
 end
